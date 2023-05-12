91438089407387dc2275c8767e633f730e6e177a
Maulana Imamul Khaq <mm930199@gmail.com>
2023-05-12 19:28:39 +0700

    initial commit

---
+++ repository/user_repository.go
@@ -0,0 +1,28 @@
+package repository
+
+import (
+	"backend2/models"
+
+	"gorm.io/gorm"
+)
+
+type UserRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) *UserRepository {
+	return &UserRepository{db}
+}
+
+func (r *UserRepository) CreateUser(user *models.User) error {
+	return r.db.Create(user).Error
+}
+
+func (r *UserRepository) GetUserByUsername(username string) (*models.User, error) {
+	var user models.User
+	err := r.db.Where("username = ?", username).First(&user).Error
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
